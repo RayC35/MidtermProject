f755eefd692bb89edada2060b486766d022fb4a7
Gabby <glsbutterfield@gmail.com>
2025-01-15 13:48:09 -0700

    create AmenityDAO, DAOImpl, and AmenityController with listAllAmenitiesByParkId and findAmenityById methods

---
+++ NationalParkCore/src/main/java/com/skilldistillery/nationalperks/data/AmenityDAO.java
@@ -0,0 +1,11 @@
+package com.skilldistillery.nationalperks.data;
+
+import java.util.List;
+
+import com.skilldistillery.nationalperks.entities.Amenity;
+
+public interface AmenityDAO {
+
+	public List<Amenity> listAllAmenitiesByParkId(int parkId);
+	public Amenity findAmenityById(int amenityId);
+}
