a953eec454242afa165ae349a664c14c3716516c
Gabby <glsbutterfield@gmail.com>
2025-01-15 17:44:47 -0700

    create editParkVisit and editAmenityVisit

--- NationalParkCore/src/main/java/com/skilldistillery/nationalperks/data/ParkVisitDAOImpl.java
+++ NationalParkCore/src/main/java/com/skilldistillery/nationalperks/data/ParkVisitDAOImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.skilldistillery.nationalperks.entities.AmenityVisit;
 import com.skilldistillery.nationalperks.entities.Park;
 import com.skilldistillery.nationalperks.entities.ParkVisit;
 import com.skilldistillery.nationalperks.entities.User;
@@ -16,16 +17,17 @@ import jakarta.transaction.Transactional;
 @Transactional
 @Service
 public class ParkVisitDAOImpl implements ParkVisitDAO {
-	
+
 	@PersistenceContext
 	private EntityManager em;
 
 	@Override
 	public List<ParkVisit> listUserParkVisits(int userId) {
 		String jpql = "SELECT p FROM ParkVisit p WHERE p.user.id = :id";
-		List<ParkVisit> userParkVisits = em.createQuery(jpql, ParkVisit.class).setParameter("id", userId).getResultList();
-		return userParkVisits;	
-		}
+		List<ParkVisit> userParkVisits = em.createQuery(jpql, ParkVisit.class).setParameter("id", userId)
+				.getResultList();
+		return userParkVisits;
+	}
 
 	@Override
 	public ParkVisit findParkVisitById(int parkVisitId) {
@@ -45,5 +47,20 @@ public class ParkVisitDAOImpl implements ParkVisitDAO {
 		em.persist(parkVisit);
 		return parkVisit;
 	}
-	
+
+	@Override
+	public ParkVisit editParkVisit(ParkVisit parkVisit, int parkVisitId) {
+		ParkVisit managedParkVisit = em.find(ParkVisit.class, parkVisitId);
+		managedParkVisit.setRemarks(parkVisit.getRemarks());
+		managedParkVisit.setRating(parkVisit.getRating());
+		managedParkVisit.setStartDate(parkVisit.getStartDate());
+		managedParkVisit.setEndDate(parkVisit.getEndDate());
+		managedParkVisit.setTitle(parkVisit.getTitle());
+		managedParkVisit.setImageURL(parkVisit.getImageURL());
+		managedParkVisit.setPublished(parkVisit.getPublished());
+		managedParkVisit.setEnabled(parkVisit.getEnabled());
+		em.persist(managedParkVisit);
+		return managedParkVisit;
+	}
+
 }
