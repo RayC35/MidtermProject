ade8808be6eb4dafadef3001439daa4fb3aa3974
Gabby <glsbutterfield@gmail.com>
2025-01-16 10:09:11 -0700

    listParkVisitsByPark get mapping and DAO/DAOImpl methods

--- NationalParkCore/src/main/java/com/skilldistillery/nationalperks/controllers/ParkVisitController.java
+++ NationalParkCore/src/main/java/com/skilldistillery/nationalperks/controllers/ParkVisitController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -62,7 +63,6 @@ public class ParkVisitController {
 		}
 	}
 	
-	
 	@GetMapping("goEditParkVisitDetails.do")
 	public String goEditParkVisitDetails(HttpSession session, @RequestParam("parkVisitId") int parkVisitToEditId) {
 		User loggedInUser = (User) session.getAttribute("loggedInUser");
@@ -87,4 +87,11 @@ public class ParkVisitController {
 		}
 	}	
 
+	@GetMapping("listParkVisitsByPark.do")
+	public String listParkVisitsByPark(Model model, @ RequestParam("parkId") int parkId) {
+		List<ParkVisit> parkVisitsByPark = parkVisitDao.listUserParkVisits(parkId);
+		model.addAttribute("parkVisitList", parkVisitsByPark);
+		return "allUserParkVisits";
+	}
+	
 }
